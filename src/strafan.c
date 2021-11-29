5826309c8debbdf76d7179a98a7f63ca7f8a7017
shodanwashere <jookaada@protonmail.com>
2021-11-29 18:26:43 +0000

    started writing the flag checks

--- src/strafan.c
+++ src/strafan.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../include/compf.h"
 #include "../include/loadbar.h"
+
+void respond_to_ip_flag();
+void respond_to_bin_seq_flag();
+void respond_to_maleable_flag();
+void respond_to_r4_flag();
+void respond_to_sp_flag();
 
 /**
  * strafan, short for "Shodan's Traffic Analyzer", is a program written as an auxiliator for a mini-project for the
@@ -9,9 +16,48 @@
  * Faculty of Sciences of the University of Lisbon (Faculdade de Ciências da Universidade de Lisboa).
  */
 void main(int argc, char* argv[]){
-	if(argc == 1){
+	if(argc < 2){
 		printf("Error: not enough args!\nUsage: strafan <filename> [OPTIONS]");
 		return;
+	}
+
+	if(check_file(argv[1])){
+		printf("Error: file does not exist or is directory");
+		return;
+	}
+
+	// Confirmed good behavior, as there are enough arguments and
+	// the file specified in argv[1] exists.
+
+	// Checking program flags
+	int ip_version = 0;
+	char* flag_seq;
+	char* m_option;
+	int r4_flag = 0;
+	int sp_flag = 0;
+	int gnuplot_flag = 0;
+	int comp_gp_flag = 0;
+	int head_number = 0;
+	if(argc > 2){
+		int i = 2;
+		while(i < argc){
+			if(!strcmp(argv[i], "-a")){
+				if(ip_version){
+					printf("Error: duplicate flag\nUsage: strafan <filename> -a <version>\n");
+					return;
+				}
+				if(i++ >= argc){
+					printf("Error: IP version not specified!\nUsage: strafan <filename> -a <version>\n");
+					return;
+				} else {
+					ip_version = atoi(argv[i]);
+					if((ip_version != 4) && (ip_version != 6)){
+						printf("Error: IP Version not valid (must be 4 or 6)\n");
+						return;
+					}
+				}
+			}
+		}
 	}
 }
 
